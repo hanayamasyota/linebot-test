69baa3c9ff7619526aa96f94afa8e4e364f505f4
unknown <Hanayama Syota@6B-28.o-hara.local>
2022-07-19 14:54:57 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 require_once __DIR__ . '/vendor/autoload.php';
 // アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv
-('CHANNEL_ACCESS_TOKEN'));
+('Rde3/I/TFVtd50VVzUEcuCswK7lIonOQL3yMZ6c5LE2sSdl/XUkLNSlAtX1w/GP1eUJ56sPTYT+R12Znwx0p/h7jBsLqe3UQHe7WejJoyjbZY/+nC5jp8L7I7IMLaennOZu1ECMwTV9MSMFDe80JnAdB04t89/1O/w1cDnyilFU='));
 // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv(
-'CHANNEL_SECRET')]);
+'f95acfbb398e6832ef3ce8734109856c')]);
 // LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader
 ::LINE_SIGNATURE];
